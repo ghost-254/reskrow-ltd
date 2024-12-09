9154b72f25b6b35371759f33517a943e9d39277f
ghost-254 <nevilleotieno6@gmail.com>
2024-12-09 23:52:58 +0300

    solved vercel errors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -89,30 +89,13 @@ module.exports = {
     					transform: 'translateX(0%)'
     				}
     			},
-    			'accordion-down': {
-    				from: {
-    					height: '0'
-    				},
-    				to: {
-    					height: 'var(--radix-accordion-content-height)'
-    				}
-    			},
-    			'accordion-up': {
-    				from: {
-    					height: 'var(--radix-accordion-content-height)'
-    				},
-    				to: {
-    					height: '0'
-    				}
-    			}
+
+
     		},
     		animation: {
     			'accordion-down': 'accordion-down 0.2s ease-out',
     			'accordion-up': 'accordion-up 0.2s ease-out',
-    			marquee: 'marquee 25s linear infinite',
-    			marquee2: 'marquee2 25s linear infinite',
-    			'accordion-down': 'accordion-down 0.2s ease-out',
-    			'accordion-up': 'accordion-up 0.2s ease-out'
+
     		}
     	}
     },
